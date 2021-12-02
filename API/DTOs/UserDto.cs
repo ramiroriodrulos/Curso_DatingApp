267e13cf4317de674e07ce841e0a92e360c53f91
ramiro rio drulos <ramiroriodrulos@hotmail.com>
2021-12-02 17:48:31 -0300

    Fin seccion 8

---
+++ API/DTOs/UserDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    /*Se devolverá este objeto cuando un usuario haga login o en registro*/
+    public class UserDto
+    {
+        public string Username { get; set; }
+        public string Token { get; set; }
+    }
+}
