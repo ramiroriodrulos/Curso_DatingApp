d450beca223ea35f85c0ff2bef299f4af22dac6f
ramiro rio drulos <ramiroriodrulos@hotmail.com>
2022-01-11 14:37:04 -0300

    Fin seccion 17

--- cliente/src/app/app.component.ts
+++ cliente/src/app/app.component.ts
@@ -1,28 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
+import { PresenceService } from './_services/presence.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+export class AppComponent implements OnInit {
+  title = 'The Dating app';
+  users: any;
 
-export class AppComponent implements OnInit{
-	
-	title = 'The Dating App';
-	users: any; //Desactivo la seguridad de tipado, users puede ser cualquier tipo, temporal.
+  constructor(private accountService: AccountService, private presence: PresenceService) {}
 
-	constructor(private accountService: AccountService){}
+  ngOnInit() {
+    this.setCurrentUser();
+  }
 
-	//Basicamente se ejcuta luego del construtor
-	ngOnInit(){
-		this.setCurrentUser();
-	}
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      this.accountService.setCurrentUser(user);
+      this.presence.createHubConnection(user);
+    }
 
-	setCurrentUser(){
-		const user: User = JSON.parse(localStorage.getItem("user"));
-		this.accountService.setCurrentUser(user);
-	}
-
+  }
 }
